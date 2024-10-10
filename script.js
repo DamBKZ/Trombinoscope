067b6d0ffe016535e86d8cf72bdfd7af3884a292
Aurélien Haye <aurelien.haye.techdeco@gmail.com>
2024-10-10 11:52:01 +0200

    first commit to setup the project

---
+++ script.js
@@ -0,0 +1,7 @@
+function sayHello(firstName, lastName){
+	console.log("Hello " + firstName + " " + lastName);
+}
+
+sayHello("Mickey", "Mouse");
+sayHello("Robert", "Doisneau");
+sayHello("Antoine", "dupont");
