442cfde49509c7006ae118fe9420bcac152a6c7e
Aurélien Haye <aurelien.haye.techdeco@gmail.com>
2024-10-10 12:33:44 +0200

    creation of ressources files

--- script.js
+++
@@ -1,7 +0,0 @@
-function sayHello(firstName, lastName){
-	console.log("Hello " + firstName + " " + lastName);
-}
-
-sayHello("Mickey", "Mouse");
-sayHello("Robert", "Doisneau");
-sayHello("Antoine", "dupont");
