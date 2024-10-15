e3e884c5bd32a0735b1bef81299097d20a8f694f
Aurélien HAYE <aurelien.haye.techdeco@gmail.com>
2024-10-15 22:58:02 +0200

    build mouseover

--- src/script.js
+++ src/script.js
@@ -1,4 +1,25 @@
-function toggleMenu() {
+/*function toggleMenu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
+
+gestion du moueover en desktop
+// Fonction pour gérer le changement d'image et l'affichage des informations
+
+// Récupération des éléments avec la classe "Photo"*/
+
+const photos = document.querySelectorAll('.Photo');
+
+photos.forEach(photo => {  // Création de la variable photo
+    const img = photo.querySelector('img'); // Sélectionne l'image dans chaque photo
+
+    // Mouseover => image en noir et blanc et afficher le bloc texte
+    photo.addEventListener('mouseover', () => {
+        img.src = img.src.replace('public/aurelien.jpg', 'public/aurelien_n-b.jpg');
+    });
+
+    // Mouseout => remettre l'image en couleur et cacher le bloc texte
+    photo.addEventListener('mouseout', () => {
+        img.src = img.src.replace('public/aurelien_n-b.jpg', 'public/aurelien.jpg');
+    });
+});
